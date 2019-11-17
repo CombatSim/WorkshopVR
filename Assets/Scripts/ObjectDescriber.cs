9bca93a0aadca89d1054e25b1ee6a1adedd3c127
Donatien DUJONCQUOY <donatiendujoncquoy@MacBook-Pro-de-Donatien.local>
2019-11-17 11:21:04 -0300

    TV Object Describer

---
+++ Assets/Scripts/ObjectDescriber.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Boo.Lang;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ObjectDescriber : MonoBehaviour
+{
+	private Hashtable objectDescriptions = new Hashtable();
+
+
+	private void Start()
+	{
+		objectDescriptions.Add("Cube", "Connect with the sphere or the Cylinder");
+		objectDescriptions.Add("Sphere", "Connect with the Cube");
+		objectDescriptions.Add("Cylinder", "Connect with the Cube");
+    }
+	private void Update()
+	{
+
+		if (Input.GetMouseButtonDown(0))
+		{
+			RaycastHit hit;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+			if (Physics.Raycast(ray, out hit, 100.0f))
+			{
+				if (hit.transform != null)
+				{
+					print(hit.transform.gameObject.name);
+					GetComponent<Text>().text = "Object: " + hit.transform.gameObject.name.ToString() + "\nInformation: " + objectDescriptions[hit.transform.gameObject.name.ToString()];
+
+				}
+			}
+		}
+	}
+
+	private void PrintName(GameObject go)
+	{
+		print(go.name);
+	}
+}
